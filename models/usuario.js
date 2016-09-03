0838a7ce5da8c0f3a28b95290069d690f27e4686
Ernande Linhares <ernandeinf@yahoo.com.br>
2016-09-03 15:27:59 -0300

    refatorando model usuario

--- models/usuario.js
+++ models/usuario.js
@@ -1,40 +1,26 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const uuid = require('node-uuid');
+const Schema   = mongoose.Schema;
+
+const id               = require('./fields/id');
+const data_criacao     = require('./fields/data_criacao');
+const data_atualizacao = require('./fields/data_atualizacao');
+const email            = require('./fields/email');
+const telefones        = require('./fields/telefones');
+const senha            = require('./fields/senha');
+
 
 const _schema = {
-    _id:          { type: String, default: uuid.v1 },
+    id,
     nome:         { type: String, required: true },
-    senha:        { type: String, required: true },
-    token:        { type: String, required: true },
     ultimo_login: { type: Date, required: true, default: Date.now },
-    email:        { 
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        match: [ /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, "E-mail inválido."] 
-    },
-    telefones: {
-        type: [ {
-            numero: {
-                type: String,
-                match: /^\d{8,9}$/,
-                required: true
-            },
-            ddd: {
-                type: String,
-                match: /^\d{2}$/,
-                required: true
-            }
-        } ],
-        default: []
-    },
-    data_criacao:     { type: Date, default: Date.now  },
-    data_atualizacao: { type: Date }
+    token:        { type: String },
+    senha,
+    email,
+    telefones,
+    data_criacao,
+    data_atualizacao
 };
 
 module.exports = mongoose.model('Usuario', new Schema(_schema) );
