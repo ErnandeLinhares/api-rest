49bfec448511f3d8191eb34986f3637304b3d919
Ernande Linhares <ernandeinf@yahoo.com.br>
2016-09-03 22:38:58 -0300

    Primeiros passos no middleware request api

--- middlewares/validateRequest.js
+++ middlewares/validateRequest.js
@@ -1,10 +1,33 @@
 'use strict';
 
-const jwt  = require('jwt-simple');
-
+const jwt                   = require('jwt-simple');
 
 module.exports = (req, res, next) => {
 
-	console.log("Middleware print URL", req.url);
-	next();
+	const token = (req.query && req.query.Bearer) || (req.headers && req.headers.Bearer);
+	
+	if(token){
+
+		try{
+
+			const decoded = jwt.decode(token, require('../configs/secret.js')());
+
+			if (decoded.exp <= Date.now()) {
+				res.status(400).json({"mensagem": "Sessão inválida." });
+				return;
+			} else {
+				
+				console.log("token decoded", decoded);
+
+				next();
+			}
+
+		}catch(e){
+			res.status(500).json({"mensagem": "Ops, erro inesperado."});
+		}
+
+	}else{
+		res.status(401).json({"mensagem": "Não autorizado"});
+	} 
+
 };
