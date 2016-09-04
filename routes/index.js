883f1fbdaeb34ca1f2d962aefa6445e510fcdff9
Ernande Linhares <ernandeinf@yahoo.com.br>
2016-09-04 12:39:15 -0300

    Alterando o esquema de rotas

--- routes/index.js
+++ routes/index.js
@@ -6,27 +6,25 @@ const Actions = require('../Actions/actions');
 
 const Routes = [
 	{
-      	method: 'get',  path: '/', action: Actions.listar
+      	method: 'get',  path: '/api/usuario/listar', action: Actions.listar
     },
     {
-      	method: 'get', path: '/:id', action: Actions.consultar
+      	method: 'get', path: '/api/usuario/:id', action: Actions.consultar
     },
     {
-      	method: 'put', path: '/:id', action: Actions.alterar
+      	method: 'put', path: '/api/usuario/:id', action: Actions.alterar
     },
     {
-      	method: 'delete', path: '/:id', action: Actions.remover
+      	method: 'delete', path: '/api/usuario/:id', action: Actions.remover
     },
     {
-		method: 'post', path: '/', action: Actions.cadastrar
+		method: 'post', path: '/api/usuario', action: Actions.cadastrar
     },
     {
     	method: 'post', path: '/sigin', action: Actions.sigin
     }
 ];
 
-Routes.forEach( (route, index) => {
-    router[route.method](route.path, route.action);
-});
+const RoutesExpress = require('./routesExpress.js')(Routes, router);
 
-module.exports = router;
+module.exports = RoutesExpress;
